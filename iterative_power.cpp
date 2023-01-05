3a008ca10c3130425acd14edc38fe63912cc9e21
RUSHABH <rushabhramani16@gmail.com>
2023-01-05 11:35:20 +0530

    add programm

---
+++ iterative_power.cpp
@@ -0,0 +1,26 @@
+
+#include <iostream>
+using namespace std;
+
+//calculate (x^n) in O(log n) 
+int power(int x, int n)
+{
+    
+    int res=1;
+    while(n>0)
+    {
+
+        if(n&1)
+            res=(res*x);
+        
+        x=(x*x); 
+        n=n>>1;
+    }
+    return res;
+}
+
+int main() {
+	int x=4,n=5;
+	cout<<power(x,n)<<endl;
+	return 0;
+}
